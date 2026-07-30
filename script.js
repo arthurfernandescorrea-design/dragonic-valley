73f2b5e081f9a5f4396a441f335039051e93d3e0
arthurfernandescorrea-design <arthur.fernandes.correa@escola.pr.gov.br>
2026-07-30 12:13:46 -0300

    alteração

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+const botoes = document.querySelectorAll("button");
 
-    const button = document. querySelectorAll("button");
-    
-    button.forEach(function(botao){
-        botao.addEventListener("click", buttonClicado);
-
-        function buttonClicado () {
-            console.log("fui clicado")
-            let texto = button.querySelector("span");
+    botoes.forEach(function(botao){
+        botao.addEventListener("click", botaoClicado);
+         
+        function botaoClicado () {
+            console.log("fui clicado");
+            let texto = botao.querySelector("span");
             texto.textContent++;
-    }    
+    }
     });
