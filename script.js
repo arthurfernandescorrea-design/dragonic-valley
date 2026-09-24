aef31d0ddb3a6f908b874ebcc10d70b8488e870b
arthurfernandescorrea-design <arthur.fernandes.correa@escola.pr.gov.br>
2026-09-24 08:02:47 -0300

    alt

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const botoes = document.querySelectorAll("button");
 
-    botoes.forEach(function(button){
-        button.addEventListener("click", botaoClicado);
+    botoes.forEach(function(botao){
+        botao.addEventListener("click", botaoClicado);
          
         function botaoClicado () {
             console.log("fui clicado");
-            let texto = button.querySelector("span");
+            let texto = botao.querySelector("span");
             texto.textContent++;
     }
     });
+
+    const bntTemaEscuro = document.querySelector(".btn-tema-escuro");
+
+    bntTemaEscuro.addEventListener(click, mudoTema);
+
+    function mudoTema() {
+        const corpoPagina = document.body;
+        if (corpoPagina.classList.contains("tema-escuro")) {
+            corpoPagina.classList.remove("tema-escuro");
+        } else {
+            corpoPagina.classList.add("tema-escuro");
+        }
+    }
